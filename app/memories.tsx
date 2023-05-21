5b777d09d87fdb16677086ff6424241977d1e91a
PedroMakengo <pedromakengomiguel@gmail.com>
2023-05-21 03:31:23 +0100

    Terminando a parte do projeto

---
+++ app/memories.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function Memories() {
+  return (
+    <View className="px-8 py-10 flex-1 items-center justify-center">
+      <Text>Memories</Text>
+    </View>
+  )
+}
